6e1ab93e119b2e3b5542f927bd9fe9a1d7208e86
Élisa Martins <elisa_cm@msn.com>
2020-11-29 12:56:50 -0500

    algo carol

--- TP3/INF8770 - TP3/code/carol.py
+++ TP3/INF8770 - TP3/code/carol.py
@@ -1,34 +1,67 @@
 import numpy as np
 import cv2
+import math
+import csv
+from matplotlib import pyplot as plt 
 
-# Create a VideoCapture object and read from input file
-# If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture('../data/video/v01.mp4')
- 
- 
-# Check if camera opened successfully
-if (cap.isOpened()== False):
-  print("Error opening video stream or file")
- 
-# Read until video is completed
-while (cap.isOpened()):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if ret == True:
- 
-        # Display the resulting frame
-        cv2.imshow('Frame', frame)
- 
-        # Press Q on keyboard to  exit
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
- 
-    # Break the loop
-    else:
-        break
- 
-# When everything done, release the video capture object
-cap.release()
- 
-# Closes all the frames
-cv2.destroyAllWindows()
+def get_euclidean_distance(hist1, hist2):
+  distance = 0
+  for j in range(len(hist1)):
+    distance += (float(hist1[j]) - float(hist2[j]))**2
+  
+  return math.sqrt(distance)
+
+def get_color_histogram(img):
+  b = [row[0] for row in cv2.calcHist([img], [0], None, [256], [0, 256])]
+  g = [row[0] for row in cv2.calcHist([img], [1], None, [256], [0, 256])]
+  r = [row[0] for row in cv2.calcHist([img], [2], None, [256], [0, 256])]
+
+  return b+g+r
+
+def get_video(image):
+  img_hist = get_color_histogram(image)
+  smallest_dist = 99999999
+  video_number = 1
+  for i in range(1, 51):
+      f = open('../data/color_histograms/' + str(i) + '.csv', 'r')
+      f_reader = csv.reader(f, delimiter=',')
+      for row in f_reader:
+        current_dist = get_euclidean_distance(row, img_hist)
+        if current_dist < smallest_dist:
+          smallest_dist = current_dist
+          video_number = i
+  return video_number
+
+def generate_indexation_file(video_number):
+  f_writer = csv.writer(open('../data/color_histograms/' + str(video_number) + '.csv', 'w', newline=''))
+  cap = cv2.VideoCapture('../data/video/v' + str(video_number).zfill(2) + '.mp4')
+
+  frame_number = 0
+  current_frame = None
+
+  while (cap.isOpened()):
+      ret, frame = cap.read()
+      
+      if ret == True:
+          frame_number += 1
+          if frame_number % 30 == 0:
+            f_writer.writerow(get_color_histogram(frame))
+
+      else:
+          break
+
+  cap.release()
+  
+def generate_indexation_files():
+  for i in range(1, 51):
+    generate_indexation_file(i)
+
+def get_solution():
+  f_writer = csv.writer(open('../data/carol_solution.csv', 'w', newline=''))
+
+  for i in range(1, 201):
+    result = get_video(cv2.imread('../data/png/i' + str(i).zfill(3) + '.png', cv2.IMREAD_COLOR))
+    f_writer.writerow([i, result])
+
+#generate_indexation_files()
+get_solution()
