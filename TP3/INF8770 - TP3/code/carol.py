59851bdf47a37ede8416a4c4afe66802440be724
unknown <elisa_cm@msn.com>
2020-12-06 21:29:21 -0500

    aa

--- TP3/INF8770 - TP3/code/carol.py
+++ TP3/INF8770 - TP3/code/carol.py
@@ -12,32 +12,16 @@ def get_euclidean_distance(hist1, hist2):
   return math.sqrt(distance)
 
 def get_color_histogram(img):
-  b = [row[0] for row in cv2.calcHist([img], [0], None, [256], [0, 256])]
-  g = [row[0] for row in cv2.calcHist([img], [1], None, [256], [0, 256])]
-  r = [row[0] for row in cv2.calcHist([img], [2], None, [256], [0, 256])]
-
-  return b+g+r
-
-def get_video(image):
-  img_hist = get_color_histogram(image)
-  smallest_dist = 99999999
-  video_number = 1
-  for i in range(1, 51):
-      f = open('../data/color_histograms/' + str(i) + '.csv', 'r')
-      f_reader = csv.reader(f, delimiter=',')
-      for row in f_reader:
-        current_dist = get_euclidean_distance(row, img_hist)
-        if current_dist < smallest_dist:
-          smallest_dist = current_dist
-          video_number = i
-  return video_number
+  return ([row[0] for row in cv2.calcHist([img], [0], None, [256], [0, 256])] + # Blue 
+          [row[0] for row in cv2.calcHist([img], [1], None, [256], [0, 256])] + # Green
+          [row[0] for row in cv2.calcHist([img], [2], None, [256], [0, 256])])  # Red
 
 def generate_indexation_file(video_number):
-  f_writer = csv.writer(open('../data/color_histograms/' + str(video_number) + '.csv', 'w', newline=''))
+  f = open('../data/color_histograms.csv', 'a', newline='')
+  f_writer = csv.writer(f)
   cap = cv2.VideoCapture('../data/video/v' + str(video_number).zfill(2) + '.mp4')
 
   frame_number = 0
-  current_frame = None
 
   while (cap.isOpened()):
       ret, frame = cap.read()
@@ -45,23 +29,47 @@ def generate_indexation_file(video_number):
       if ret == True:
           frame_number += 1
           if frame_number % 30 == 0:
-            f_writer.writerow(get_color_histogram(frame))
+            f_writer.writerow([video_number, frame_number/30, get_color_histogram(frame)])
 
       else:
           break
 
   cap.release()
+  f.close()
   
 def generate_indexation_files():
+  f = open('../data/color_histograms.csv', 'w', newline='')
+  f_writer = csv.writer(f)
+  f_writer.writerow(["video number", "time in s", "color histogram"])
+  f.close()
   for i in range(1, 51):
     generate_indexation_file(i)
 
-def get_solution():
-  f_writer = csv.writer(open('../data/carol_solution.csv', 'w', newline=''))
+def get_video(image):
+  img_hist = get_color_histogram(image)
 
-  for i in range(1, 201):
-    result = get_video(cv2.imread('../data/png/i' + str(i).zfill(3) + '.png', cv2.IMREAD_COLOR))
-    f_writer.writerow([i, result])
+  smallest_dist = 99999999
+  video_number = 1
+  f = open('../data/color_histograms.csv', 'r')
+  f_reader = csv.reader(f, delimiter=',')
+  f.readline()
+  for row in f_reader:
+    current_dist = get_euclidean_distance(eval(row[2]), img_hist)
+    if current_dist < smallest_dist:
+       smallest_dist = current_dist
+       video_number = row[0]
+  
+  return video_number
+
+def get_solution():
+  image_types = ["jpeg", "png"]
+
+  for ext in image_types:
+    f_writer = csv.writer(open('../data/carol_solution_' + ext + '.csv', 'w', newline=''))
+
+    for i in range(1, 201):
+      result = get_video(cv2.imread('../data/' + ext + '/i' + str(i).zfill(3) + '.' + ext, cv2.IMREAD_COLOR))
+      f_writer.writerow([i, result])
 
 #generate_indexation_files()
 get_solution()
